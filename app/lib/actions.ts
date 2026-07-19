bb00756d43b22251ff37a9be930e0e7f06395b3d
Maksim Beliavsky <maksimbell503@gmail.com>
2026-07-19 21:57:59 +0300

    feat: add signup session flow

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,9 @@
 
 import { neon } from "@neondatabase/serverless";
 import { generateSlug } from "./slug";
+import { FormState, SignupFormSchema } from "./definitions";
+import { createSession } from "./sessions";
+import bcrypt from "bcrypt";
 
 const sql = neon(`${process.env.DATABASE_URL}`);
 
@@ -13,4 +16,44 @@ export async function createLink(formdata: FormData): Promise<string> {
             VALUES (${slug}, ${original_url}, now() + interval '30 days')`;
 
   return slug;
+}
+
+export async function signup(state: FormState, formData: FormData) {
+  const validatedFields = SignupFormSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return { errors: validatedFields.error.flatten().fieldErrors };
+  }
+
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  let user = null;
+
+  try {
+    const data =
+      await sql`INSERT INTO users (name, email, password_hash, created_at)
+            VALUES (${name}, ${email}, ${hashedPassword}, now())
+            RETURNING id, name, email, created_at`;
+
+    user = data[0];
+  } catch (error) {
+    if (
+      typeof error === "object" &&
+      error !== null &&
+      "code" in error &&
+      error.code === "23505"
+    ) {
+      return { errors: { email: ["Email is already is use"] } };
+    }
+
+    return {
+      message: "An error occurred while creating your account.",
+    };
+  }
+
+  await createSession(user.id);
 }
---
+++ app/lib/sessions.ts
@@ -0,0 +1,58 @@
+import { neon } from "@neondatabase/serverless";
+import { jwtVerify, SignJWT } from "jose";
+import { createHash, randomBytes } from "node:crypto";
+import { SessionPayload } from "./definitions";
+import { cookies } from "next/headers";
+
+export function generateSessionToken() {
+  return randomBytes(32).toString("base64url");
+}
+
+export function hashSessionToken(token: string) {
+  return createHash("sha256").update(token).digest("hex");
+}
+
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export async function encrypt(payload: SessionPayload) {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(encodedKey);
+}
+
+export async function decrypt(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch {
+    console.log("Failed to verify session");
+  }
+}
+
+const sql = neon(`${process.env.DATABASE_URL}`);
+
+export async function createSession(userId: string) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+  const hashedToken = hashSessionToken(generateSessionToken());
+  const data = await sql`INSERT INTO sessions (user_id, token_hash, expires_at)
+      VALUES (${userId}, ${hashedToken}, ${expiresAt})
+      RETURNING id`;
+
+  const sessionId = data[0].id;
+  const session = await encrypt({ sessionId, expiresAt });
+
+  const cookieStore = await cookies();
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    secure: true,
+    expires: expiresAt,
+    sameSite: "lax",
+    path: "/",
+  });
+}
